ce8b7121ea5b3650f11eaf916e2c94927beafba1
Denis Kintsel <moto.saratov164.zon@gmail.com>
2022-09-13 17:46:46 +0400

    KintselProduction

--- pattern_architecture_EdyPystoy_stage_2/lib/app/presentation/pages/autorization/cubit/autorization_state.dart
+++
@@ -1,8 +0,0 @@
-part of 'autorization_cubit.dart';
-
-@freezed
-abstract class AuthState with _$AuthState {
-  const factory AuthState.initial() = Initial;
-  const factory AuthState.loading() = Loading;
-  const factory AuthState.error() = Error;
-}
--- pattern_architecture_EdyPystoy_stage_2/lib/app/presentation/pages/registration/cubit/registration_state.dart
+++
@@ -1,8 +0,0 @@
-part of 'registration_cubit.dart';
-
-@freezed
-abstract class RegistrationState with _$RegistrationState {
-  const factory RegistrationState.initial() = Initial;
-  const factory RegistrationState.loading() = Loading;
-  const factory RegistrationState.error() = Error;
-}
